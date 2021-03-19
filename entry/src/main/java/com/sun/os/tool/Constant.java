43cae461c281638c174ff42302c9a9aceefa00a3
suzhonxia <suzhonxia@163.com>
2021-03-19 10:33:40 +0800

    Update Constant.java

--- entry/src/main/java/com/sun/os/tool/Constant.java
+++ entry/src/main/java/com/sun/os/tool/Constant.java
@@ -1,6 +1,5 @@
 package com.sun.os.tool;
 
 public class Constant {
-   public static final String posterPath = "http://media4jbkk.oss-cn-qingdao.aliyuncs.com/testing/poster.png?" +
-           "OSSAccessKeyId=LTAIXSfYgg4kO8HF&Expires=1711994817&Signature=NLSd7RNGcLICpFSRgRBjZYtHNcU%3D";
+   public static final String posterPath = "";
 }
